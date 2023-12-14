ef8b80c2fc578127b84fb2a845944cef421b254c
47224 - André Graça <A47224@alunos.isel.pt>
2023-12-14 23:20:55 +0000

    github milestones integrated with google tasks

--- taskhub/src/controllers/githubController.js
+++ taskhub/src/controllers/githubController.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const githubService = require("../services/github/githubService");
+const googleTasksService = require("../services/google/googleTasksService");
 const router = express.Router();
 
 router.get("/github/repos", async (req, rsp) => {
   const { google, github } = req.cookies;
-  const repos = await githubService.getReposFromUser(github.token);
-  rsp.render("repos", { repos, google, github });
+
+  const reposPromise = githubService.getReposFromUser(github.token);
+  const listsPromise = googleTasksService.getListsFromUser(google.token);
+  const [repos, lists] = await Promise.all([reposPromise, listsPromise]);
+
+  rsp.render("repos", { repos, lists, google, github });
 });
 
 module.exports = router;
